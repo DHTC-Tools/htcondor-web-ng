082957eb0190e7c250a1f6bf8a3244a99e56baf4
Lincoln Bryant <lincolnb@hep.uchicago.edu>
2013-06-21 15:36:50 -0500

    added key index

--- backend.py
+++ backend.py
@@ -26,7 +26,7 @@ r_server =redis.Redis(args.redserver)
 # Let's set the timestamp outside of the loop, such that each time we run the 
 # backend, all nodes report the same timestamp. 
 timestamp = str(int(time.time()))
-keyIndex = []
+KeyIndexVal == []
 # This forms the data structure and pushes it into redis as a list.
 for slot in slotState[:]:
   key = appId + ":" + slot['Name'] + ":" + timestamp 
@@ -37,10 +37,12 @@ for slot in slotState[:]:
   for entry in value:
       # we could probably make this faster by pipelining
       r_server.lpush(key,entry)
-  # Add the list of keys to an index
-  keyIndex.append(key)
+  # Add the list of keys to an index (now the keys are values O_o)
+  # better naming needed..
+  keyIndexVal.append(key)
 
-print keyIndex
+keyIndexKey = appId + ":index:latest" # bad naming scheme or _worst_ name scheme?
 # need some logic to check if the key exists
 # suspect it will fail if the key does not initially exist
-r_server.lset("calliope:index:latest",0,keyIndex)
+if r_server.exists("") == "True" 
+r_server.lset(keyIndexKey,0,keyIndexValue)
